74443a71e61bdb231063571b94477ec78146f992
sharvesh-10 <sharveshsofficial@gmail.com>
2020-09-01 16:32:17 +0530

    Added projects

--- js/script.js
+++ js/script.js
@@ -81,4 +81,32 @@ $(document).ready(function(){
 			body.removeClass("fixedNav");
 		}
 	}
+
+	$(".items").isotope({
+		filter:'*',
+		animationOptions:{
+			duration:1500,
+			easing:'linear',
+			queue: false
+		}
+	})
+
+	$("#filters a").click(function() {
+
+		$("#filters .current").removeClass("current");
+		$(this).addClass("current");
+
+		var selector = $(this).attr("data-filter");
+
+		$(".items").isotope({
+			filter: selector,
+			animationOptions: {
+				duration: 1500,
+				easing: 'linear',
+				queue: false
+			}
+		});
+
+		return false;
+	});
 });
